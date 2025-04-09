aeac5464746b1b35e1b2c9c6eff6e0af05c3d74b
Har1sh-k <hkolla03@gmail.com>
2025-04-09 11:39:12 -0500

    download working

---
+++ src/download_json.py
@@ -0,0 +1,56 @@
+import requests
+import datetime
+
+import status_logger
+
+       
+HEADERS = {
+            "Accept": "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+            "Origin": "https://grants.gov",
+            "Referer": "https://grants.gov/",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.140 Safari/537.36"
+            }
+        
+PAYLOAD = {
+        "keyword": None,
+        "cfda": None,
+        "agencies": None,
+        "sortBy": "openDate|desc",
+        "rows": 5000,
+        "eligibilities": None,
+        "fundingCategories": None,
+        "fundingInstruments": None,
+        "dateRange": "",
+        "oppStatuses": "forecasted|posted"
+        }
+
+def gen_grants():
+    success = False
+    status_logger.logger("info","Downloading CSV file...")
+    try:
+        url = "https://micro.grants.gov/rest/opportunities/search_export_Mark2"
+        response = requests.get(url, stream=True)
+ 
+        
+        response = requests.post(url, headers=HEADERS, json=PAYLOAD)
+        
+        if response.status_code == 200:
+            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            with open(f"src/grants_data/grants_data_{timestamp}.json", "wb") as file:                
+                file.write(response.content)
+            status_logger.logger("info",f"CSV file downloaded successfully as 'grants_data_{timestamp}.json'.")
+            success = True
+        
+        else:
+            status_logger.logger("error",f"Failed to download CSV file. HTTP Status Code:", response.status_code)
+        
+    except requests.exceptions.RequestException as e:
+        status_logger.logger("error",f"Error downloading {url}: {e}")
+    except Exception as e:
+        status_logger.logger("error",f"Error writing to file - grants_data.json: {e}")
+
+    return success
+
+if __name__ == "__main__":
+    gen_grants()
